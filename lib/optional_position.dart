fe80adcfd0f6a3a127f1383fe26e5ae7c0355715
Poommipat7 <poommipat462@gmail.com>
2025-11-21 11:12:17 +0700

    update4

---
+++ lib/optional_position.dart
@@ -0,0 +1,9 @@
+void printInfo(String name, String gender, [String? title]) {
+  print("Hello $name your gender is $gender");
+}
+
+void callPrint() {
+  printInfo("John", 'Male');
+  printInfo("John", 'Male',"Mr.");
+  printInfo("John", 'Male',"Ms.");
+}
